48d6ad1d6b40691e6a86c865af6d0b77e113a7c7
Danielle Barker <caseybarker86@gmail.com>
2024-11-24 20:01:30 -0700

    status check for draw things

---
+++ control/control.go
@@ -0,0 +1,76 @@
+package main
+
+import (
+	"encoding/json"
+	"net/http"
+
+	"github.com/go-vgo/robotgo"
+)
+
+var (
+	scrollX = 280
+	scrollY = 430
+	buttonX = 324
+	buttonY = 508
+)
+
+func openDrawThings(w http.ResponseWriter, r *http.Request) {
+
+	robotgo.Run("open /Applications/Draw\\ Things.app")
+	robotgo.MilliSleep(2000)
+	robotgo.MoveRelative(-1000, -1000)
+	robotgo.MilliSleep(1000)
+
+	robotgo.Move(scrollX, scrollY)
+	robotgo.Click("left")
+	robotgo.MilliSleep(1000)
+	robotgo.ScrollDir(100000, "down")
+	robotgo.Move(buttonX, buttonY)
+	robotgo.MilliSleep(1000)
+	robotgo.Click("left")
+	bit, err := robotgo.CaptureImg(scrollX, scrollY, 100, 100)
+	if err != nil {
+		println("CaptureImg error")
+	}
+	robotgo.Save(bit, "drawthings.png", 100)
+	// send image to frontend, now
+	// ok go
+	http.ServeFile(w, r, "drawthings.png")
+
+}
+
+func checkStatus(w http.ResponseWriter, r *http.Request) {
+	var windowId int
+	robotgo.Run("open /Applications/Draw\\ Things.app")
+	robotgo.MilliSleep(2000)
+	robotgo.MoveRelative(-1000, -1000)
+	robotgo.MilliSleep(1000)
+
+	robotgo.Move(scrollX, scrollY)
+	robotgo.Click("left")
+	robotgo.MilliSleep(1000)
+	robotgo.ScrollDir(100000, "down")
+	// set window to top
+	ids, err := robotgo.FindIds("drawthings")
+	if err != nil || len(ids) == 0 {
+		println("window not found")
+		// respond with {"status": "closed"}
+		response := map[string]string{"status": "closed"}
+		json.NewEncoder(w).Encode(response)
+
+	} else {
+		windowId = ids[0]
+		robotgo.ActivePid(windowId)
+		robotgo.MilliSleep(2000)
+		// respond with {"status": "open"}
+		bit, err := robotgo.CaptureImg(scrollX, scrollY, 100, 100)
+		if err != nil {
+			println("CaptureImg error")
+		}
+		robotgo.Save(bit, "drawthings.png", 100)
+		// send image to frontend, now
+		// ok go
+		http.ServeFile(w, r, "drawthings.png")
+
+	}
+}
---
+++ control/main.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+	"net/http"
+	"os"
+)
+
+var apiMux *http.ServeMux
+var err error
+
+func main() {
+	setupApi()
+	http.Handle("/api/", http.StripPrefix("/api", apiMux))
+
+	http.HandleFunc("/", serveFrontend)
+	http.HandleFunc("/assets/", func(w http.ResponseWriter, r *http.Request) {
+		http.FileServer(http.Dir("./public")).ServeHTTP(w, r)
+	})
+	fmt.Printf("listening on port 3334\n")
+	err = http.ListenAndServe(":3334", nil)
+	if errors.Is(err, http.ErrServerClosed) {
+		fmt.Printf("server closed\n")
+	} else if err != nil {
+		fmt.Printf("error starting server: %s\n", err)
+		os.Exit(1)
+	} else {
+		fmt.Printf("server started on port 3333\n")
+	}
+
+}
+
+func serveFrontend(w http.ResponseWriter, r *http.Request) {
+	fmt.Printf("serving frontend\n")
+
+	http.FileServer(http.Dir("./dist")).ServeHTTP(w, r)
+}
+
+func setupApi() {
+	apiMux = http.NewServeMux()
+	apiMux.HandleFunc("/check-status", checkStatus)
+	apiMux.HandleFunc("/open-draw-things", openDrawThings)
+}
