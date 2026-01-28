bc74ef315a4fdf080198a48fdf1206529111d2ce
Danielle Barker <caseybarker86@gmail.com>
2026-01-28 12:16:59 +0000

    new

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/zsh
-DIR=/Users/daniellebarker/programming/personal/draw-go-react
+DIR=~/programming/personal/draw-go-react
 # delete everything in the public folder except for the images folder
 
 # Delete everything inside ./backend/public except the ./backend/public/images directory and its contents
